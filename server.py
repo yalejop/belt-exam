97fea524fe347d9a29f089e238aa47465939de30
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-26 11:31:38 -0500

    initializing Flask Project

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask_app import app
-
-#importando controlador
-from flask_app.controllers import users_controller
-from flask_app.controllers import appointments_controller
-
-if __name__=='__main__':
-    app.run(debug=True)
