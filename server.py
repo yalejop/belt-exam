6012b343a8a780f8122b2a9d91e3deadbe9deb51
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-08-27 23:06:03 -0500

    create a template for create ice creams and a model for orders

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask_app import app
 
 #importando controlador
 from flask_app.controllers import users_controller
+from flask_app.controllers import orders_controller
 
 if __name__=='__main__':
     app.run(debug=True)
